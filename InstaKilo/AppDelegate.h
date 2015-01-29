df4779006502e98ec626a2bef329bb53e0c97ee0
Renato Camilio <renatomcamilio@gmail.com>
2015-01-29 10:50:18 -0800

    Initial Commit

---
+++ InstaKilo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstaKilo
+//
+//  Created by Renato Camilio on 1/29/15.
+//  Copyright (c) 2015 Renato Camilio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InstaKilo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstaKilo
+//
+//  Created by Renato Camilio on 1/29/15.
+//  Copyright (c) 2015 Renato Camilio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
